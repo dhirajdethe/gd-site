87c4bea5b1154bc82f42b1d1cb5a6eb9a2236f60
Tuhin Bhuyan <tuhin.bhuyan0@gmail.com>
2017-11-11 03:34:53 +0530

    Adjusts height

--- resources.php
+++ resources.php
@@ -229,11 +229,11 @@
 <script type="text/javascript" src="script/script.js"></script>
 
 <script type="text/javascript">
-    var devnagriSectionHeight = $("#resources-section-1").offset().top - 70;
-    var typographySectionHeight = $("#resources-section-2").offset().top - 70;
-    var historiographySectionHeight = $("#resources-section-3").offset().top - 70;
-    var bookspapersSectionHeight = $("#resources-section-4").offset().top - 70;
-    var latintypeSectionHeight = $("#resources-section-5").offset().top - 70;
+    var devnagriSectionHeight = $("#resources-section-1").offset().top - 50;
+    var typographySectionHeight = $("#resources-section-2").offset().top - 50;
+    var historiographySectionHeight = $("#resources-section-3").offset().top - 50;
+    var bookspapersSectionHeight = $("#resources-section-4").offset().top - 50;
+    var latintypeSectionHeight = $("#resources-section-5").offset().top - 50;
 
     $(window).scroll(function(){
         if (scroll > devnagriSectionHeight) {
