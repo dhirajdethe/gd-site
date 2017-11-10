3276c90f6e48ddcd7ca7e53e1a87b66116f389d2
Tuhin Bhuyan <tuhin.bhuyan0@gmail.com>
2017-11-11 03:33:43 +0530

    Fix on height

--- resources.php
+++ resources.php
@@ -229,11 +229,11 @@
 <script type="text/javascript" src="script/script.js"></script>
 
 <script type="text/javascript">
-    var devnagriSectionHeight = $("#resources-section-1").offset().top - 180;
-    var typographySectionHeight = $("#resources-section-2").offset().top - 180;
-    var historiographySectionHeight = $("#resources-section-3").offset().top - 180;
-    var bookspapersSectionHeight = $("#resources-section-4").offset().top - 180;
-    var latintypeSectionHeight = $("#resources-section-5").offset().top - 180;
+    var devnagriSectionHeight = $("#resources-section-1").offset().top - 70;
+    var typographySectionHeight = $("#resources-section-2").offset().top - 70;
+    var historiographySectionHeight = $("#resources-section-3").offset().top - 70;
+    var bookspapersSectionHeight = $("#resources-section-4").offset().top - 70;
+    var latintypeSectionHeight = $("#resources-section-5").offset().top - 70;
 
     $(window).scroll(function(){
         if (scroll > devnagriSectionHeight) {
