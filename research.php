d26ee0cd1a9b9fd96ae1d83c6caf1bfed1490082
savshikha <savshikha@gmail.com>
2018-02-08 13:46:19 +0530

    hyperlinks edit

--- research.php
+++ research.php
@@ -83,7 +83,7 @@
 
                   -->
                         <li class="publication-list-items publication-category-3">
-                          <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007%2F978-3-642-11762-6_12">“Development of an Intuitive User-Centric Font Selection Menu.”</a>; <a class="link" target="_blank" href="https://link.springer.com/book/10.1007/978-3-642-11762-6">  Human Work Interaction Design: Usability in Social, Cultural and Organizational Contexts pp 144-153. Springer, 2010. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                          <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007%2F978-3-642-11762-6_12">“Development of an Intuitive User-Centric Font Selection Menu.”</a>; <a class="link" target="_blank" href="https://link.springer.com/book/10.1007/978-3-642-11762-6">  Human Work Interaction Design: Usability in Social, Cultural and Organizational Contexts</a> pp 144-153. Springer, 2010.&nbsp;&nbsp;-&nbsp;&nbsp;
                           Dalvi, Girish.
                         </li>
 
@@ -193,103 +193,103 @@
                         </li>
 
                         <li class="publication-list-items publication-category-2">
-                          <a class="link" target="_blank" href="http://www.idc.iitb.ac.in/resources/dt-jan-2009/Anatomy%20of%20Devanagari.pdf">“Anatomy of Devanagari Typefaces”</a>, <a class="link" target="_blank" href="http://www.idc.iitb.ac.in/resources/design-thoughts.html">Design Thoughts, pp 30–36 2009. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                          <a class="link" target="_blank" href="http://www.idc.iitb.ac.in/resources/dt-jan-2009/Anatomy%20of%20Devanagari.pdf">“Anatomy of Devanagari Typefaces”</a>, <a class="link" target="_blank" href="http://www.idc.iitb.ac.in/resources/design-thoughts.html">Design Thoughts</a>, pp 30–36 2009.&nbsp;&nbsp;-&nbsp;&nbsp;
                           Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                          <a class="link" target="_blank" href="http://dl.acm.org/citation.cfm?id=3025944">“Shift+Tap or Tap+LongPress?: The Upper Bound of Typing Speed on InScript.”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=3025453&picked=source&CFID=843819062&CFTOKEN=55063656"> Proceedings of the 2017 CHI Conference on Human Factors in Computing Systems (CHI ‘17). ACM, New York, NY, USA, 2059-2063. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                          <a class="link" target="_blank" href="http://dl.acm.org/citation.cfm?id=3025944">“Shift+Tap or Tap+LongPress?: The Upper Bound of Typing Speed on InScript.”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=3025453&picked=source&CFID=843819062&CFTOKEN=55063656"> Proceedings of the 2017 CHI Conference on Human Factors in Computing Systems (CHI ‘17)</a>. ACM, New York, NY, USA, 2059-2063.&nbsp;&nbsp;-&nbsp;&nbsp;
                           Ghosh S., Joshi A., Joshi M, Emmadi N, Dalvi G., et.al.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="http://ieeexplore.ieee.org/document/7939261/?reload=true">“An. — An interactive full body exercise experience for patients suffering from Ankylosing Spondylitis,”</a>, <a class="link" target="_blank"href="http://www.segah.org/2017/"> 2017 IEEE 5th International Conference on Serious Games and Applications for Health (SeGAH), Perth, WA, 2017, pp. 1-8. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="http://ieeexplore.ieee.org/document/7939261/?reload=true">“An. — An interactive full body exercise experience for patients suffering from Ankylosing Spondylitis,”</a>, <a class="link" target="_blank"href="http://www.segah.org/2017/"> 2017 IEEE 5th International Conference on Serious Games and Applications for Health (SeGAH)</a>, Perth, WA, 2017, pp. 1-8. &nbsp;&nbsp;-&nbsp;&nbsp;
                         Bandyopadhyay J., Dalvi G.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="http://www.typoday.in/2017/spk_papers/maithili-shingre-typoday-2017.pdf">“Reviving a Manuscript Style: The design process of Jaini”</a>, <a class="link" target="_blank"href="http://typoday.in/2017/">Typography Day 2017,  Department of Integrated Design, University of Moratuwa, Colombo, Sri Lanka 2017.</a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="http://www.typoday.in/2017/spk_papers/maithili-shingre-typoday-2017.pdf">“Reviving a Manuscript Style: The design process of Jaini”</a>, <a class="link" target="_blank"href="http://typoday.in/2017/">Typography Day 2017</a>,  Department of Integrated Design, University of Moratuwa, Colombo, Sri Lanka 2017.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Shingre M., Dalvi G.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007/978-981-10-3521-0_20">“Can Interactive Installations Bring About Behaviour Change? Using Interactive Installation to Change Food Waste Behaviours”</a>, <a class="link" target="_blank"href="http://www.springer.com/in/book/9789811035203">Research into Design for Communities, Volume 2. ICoRD 2017. Smart Innovation, Systems and Technologies, vol 66. Springer, Singapore 2017. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007/978-981-10-3521-0_20">“Can Interactive Installations Bring About Behaviour Change? Using Interactive Installation to Change Food Waste Behaviours”</a>, <a class="link" target="_blank"href="http://www.springer.com/in/book/9789811035203">Research into Design for Communities, Volume 2. ICoRD 2017</a>. Smart Innovation, Systems and Technologies, vol 66. Springer, Singapore 2017. &nbsp;&nbsp;-&nbsp;&nbsp;
                         Bandyopadhyay J., Dalvi G.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2935366">“Does prediction really help in Marathi text input?: empirical analysis of a longitudinal study.”</a>, <a class="link" target="_blank"href="https://mobilehci.acm.org/2016/">18th International Conference on Human Computer Interaction with Mobile Devices and Services MobileHCI’16, Florence, Italy</a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2935366">“Does prediction really help in Marathi text input?: empirical analysis of a longitudinal study.”</a>, <a class="link" target="_blank"href="https://mobilehci.acm.org/2016/">18th International Conference on Human Computer Interaction with Mobile Devices and Services MobileHCI’16</a>, Florence, Italy&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, G., et al.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2835970">“A Protocol to Evaluate Virtual Keyboards for Indian Languages”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=2835966&picked=prox">Proceedings of the 7th International Conference on HCI, IndiaHCI 2015, Guwahati, India. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2835970">“A Protocol to Evaluate Virtual Keyboards for Indian Languages”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=2835966&picked=prox">Proceedings of the 7th International Conference on HCI, IndiaHCI 2015</a>, Guwahati, India. &nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, G., et al.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://www.researchgate.net/publication/291350539_CAN_FUN_AND_PURPOSE_COEXIST_IN_A_LEARNING_GAME_A_CASE_STUDY_OF_A_NOVEL_GEOMETRY_LEARNING_GAME">“Can Fun And Purpose Coexist in a Learning Game? A Case Study Of A Novel Geometry Learning Game”</a>, <a class="link" target="_blank"href="https://library.iated.org/publications/EDULEARN15/start/400"> EDULEARN15 2015, Barcelona, Spain pp. 2839–2848.</a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://www.researchgate.net/publication/291350539_CAN_FUN_AND_PURPOSE_COEXIST_IN_A_LEARNING_GAME_A_CASE_STUDY_OF_A_NOVEL_GEOMETRY_LEARNING_GAME">“Can Fun And Purpose Coexist in a Learning Game? A Case Study Of A Novel Geometry Learning Game”</a>, <a class="link" target="_blank"href="https://library.iated.org/publications/EDULEARN15/start/400"> EDULEARN15 2015</a>, Barcelona, Spain pp. 2839–2848.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Athavale S., Johry A., Dalvi G.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="http://www.typoday.in/2015/spk_papers/Pradnya_Naik-Typographyday2015.pdf">“Calligraphic Analysis of the Jain Manuscript Style”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2015/index.html">Typography Day 2015, IDC, IIT Bombay, 2015, Mumbai. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="http://www.typoday.in/2015/spk_papers/Pradnya_Naik-Typographyday2015.pdf">“Calligraphic Analysis of the Jain Manuscript Style”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2015/index.html">Typography Day 2015</a>, IDC, IIT Bombay, 2015, Mumbai. &nbsp;&nbsp;-&nbsp;&nbsp;
                         Naik P, Dalvi G.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2676702.2676705">“Corpus of Marathi Word Frequencies from Touch-Screen Devices Using Swarachakra Android Keyboard,”</a>, <a class="link" target="_blank"href="http://hcibib.org/IndiaHCI14">Proceedings of the India HCI 2014 Conference on Human Computer Interaction IndiaHCI ‘14, 2014, Delhi, India pp 74–79. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2676702.2676705">“Corpus of Marathi Word Frequencies from Touch-Screen Devices Using Swarachakra Android Keyboard,”</a>, <a class="link" target="_blank"href="http://hcibib.org/IndiaHCI14">Proceedings of the India HCI 2014 Conference on Human Computer Interaction IndiaHCI ‘14</a>, 2014, Delhi, India pp 74–79. &nbsp;&nbsp;-&nbsp;&nbsp;
                         Joshi A, Dalvi G, et.al
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2037422">“Design And Evaluation Of Devanagari Virtual Keyboards For Touch Screen Mobile Phones”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=2037373&picked=prox">13th International Conference on Human Computer Interaction with Mobile Devices and Services MobileHCI’11 Stockholm, Sweden pp 323–332. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://dl.acm.org/citation.cfm?id=2037422">“Design And Evaluation Of Devanagari Virtual Keyboards For Touch Screen Mobile Phones”</a>, <a class="link" target="_blank"href="https://dl.acm.org/citation.cfm?id=2037373&picked=prox">13th International Conference on Human Computer Interaction with Mobile Devices and Services MobileHCI’11</a> Stockholm, Sweden pp 323–332.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Joshi, A., Dalvi, G., et.al.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="http://www.iasdr2009.or.kr/Papers/Orally%20Presented%20Papers/Design%20Method/A%20method%20to%20capture%20categorization%20strategies%20in%20order%20to%20understand%20user%20perception.%20A%20case%20study%20on%20user%20categorization%20strategies%20for%20typefaces.pdf">“A Method to Capture Categorization Strategies in order to Understand User Perception: A case study on User Categorization Strategies for Typefaces.”</a>, <a class="link" target="_blank"href="http://www.iasdr.org/node/15">International Association of Societies of Design Research. Seoul, South Korea 2009, pp 1855–1865.</a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="http://www.iasdr2009.or.kr/Papers/Orally%20Presented%20Papers/Design%20Method/A%20method%20to%20capture%20categorization%20strategies%20in%20order%20to%20understand%20user%20perception.%20A%20case%20study%20on%20user%20categorization%20strategies%20for%20typefaces.pdf">“A Method to Capture Categorization Strategies in order to Understand User Perception: A case study on User Categorization Strategies for Typefaces.”</a>, <a class="link" target="_blank"href="http://www.iasdr.org/node/15">International Association of Societies of Design Research</a>. Seoul, South Korea 2009, pp 1855–1865.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-11762-6_12">“Development of Font Selection Menu for Devanagari.”</a>, <a class="link" target="_blank"href=" ">Second IFIP Working Group 13.6 Conference, HWID. Pune, 2009. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-642-11762-6_12">“Development of Font Selection Menu for Devanagari.”</a>, <a class="link" target="_blank"href=" ">Second IFIP Working Group 13.6 Conference</a>, HWID. Pune, 2009.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-1">
-                        <a class="link" target="_blank" href=" ">“Evaluation of Devanagari Typefaces for Onscreen Legibility.”</a>, <a class="link" target="_blank"href=" "> National Workshop on Calligraphy and Typography, CDAC. Pune, 2007. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href=" ">“Evaluation of Devanagari Typefaces for Onscreen Legibility.”</a>, <a class="link" target="_blank"href=" "> National Workshop on Calligraphy and Typography</a>, CDAC. Pune, 2007.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href="https://www.youtube.com/watch?v=-pMxWn_9clU">“Ek Mukta Font Family”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2015/">Typography Day 2015. Mumbai, 2015. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://www.youtube.com/watch?v=-pMxWn_9clU">“Ek Mukta Font Family”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2015/">Typography Day 2015</a>. Mumbai, 2015.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href="https://www.youtube.com/watch?v=xvT9BPDRSmA">“Devanagari Font Families & Glyph Counts”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2012/">Typography Day 2012. Mumbai, 2012. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="https://www.youtube.com/watch?v=xvT9BPDRSmA">“Devanagari Font Families & Glyph Counts”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2012/">Typography Day 2012</a>. Mumbai, 2012.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href=" ">“Visual Literacy of Devanagari Typography”</a>, <a class="link" target="_blank"href="http://istvc.org/ictvc-conference.html">4th International Conference on Typography and Visual Communication (ICTVC). Nicosia, 2010. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href=" ">“Visual Literacy of Devanagari Typography”</a>, <a class="link" target="_blank"href="http://istvc.org/ictvc-conference.html">4th International Conference on Typography and Visual Communication (ICTVC)</a>. Nicosia, 2010.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href="http://aksharaya.org/event/aksharsanvad-a-talk-by-girish-dalvi/">“Technological Struggles in Indian Typographic Traditions”</a>, <a class="link" target="_blank"href="http://aksharaya.org/en/landing/">Aksharaya Invited Guest Lecture, Sir JJ Institute of Applied Art, Mumbai 2010. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href="http://aksharaya.org/event/aksharsanvad-a-talk-by-girish-dalvi/">“Technological Struggles in Indian Typographic Traditions”</a>, <a class="link" target="_blank"href="http://aksharaya.org/en/landing/">Aksharaya</a> Invited Guest Lecture, Sir JJ Institute of Applied Art, Mumbai 2010.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href=" ">“Problems and Opportunities in Devanagari Typography”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2009/">Typography Day 2009. Pune, 2009. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href=" ">“Problems and Opportunities in Devanagari Typography”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2009/">Typography Day 2009</a>. Pune, 2009.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
                         <li class="publication-list-items publication-category-3">
-                        <a class="link" target="_blank" href=" ">“Classification of Devanagari Typefaces”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2008/">Typography Day 2008. Mumbai, 2008. </a>&nbsp;&nbsp;-&nbsp;&nbsp;
+                        <a class="link" target="_blank" href=" ">“Classification of Devanagari Typefaces”</a>, <a class="link" target="_blank"href="http://www.typoday.in/2008/">Typography Day 2008</a>. Mumbai, 2008.&nbsp;&nbsp;-&nbsp;&nbsp;
                         Dalvi, Girish.
                         </li>
 
